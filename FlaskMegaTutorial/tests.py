8f2d199c28d6c805ca35cb19425fca450416fe69
Edgar S. Hurtado <edsanhu@gmail.com>
2018-06-19 19:53:01 +0200

    Finish part XV

--- FlaskMegaTutorial/tests.py
+++ FlaskMegaTutorial/tests.py
@@ -1,19 +1,26 @@
-from app import db
+from app import db, create_app
 from app.models import User, Post
 from datetime import datetime, timedelta
 from flask import current_app
 import unittest
+from config import Config
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'  # Makes SQLAlchemy to use an in-memory SQLite database during the tests
 
 
 class UserModelCase(unittest.TestCase):
     def setUp(self):
-        current_app.config['SQLALCHEMY_DATABASE_URI'] = \
-            'sqlite://'  # Makes SQLAlchemy to use an in-memory SQLite database during the tests
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
         db.create_all()
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
+        self.app_context.pop()
 
     def test_password_hashing(self):
         u = User(username='susan')
