165babb89768294a322caddeef7226e105fc0705
Edgar S. Hurtado <edsanhu@gmail.com>
2018-09-07 18:43:47 +0200

    Add url resolver and file loader
    Clase 2. Minuto 55

--- WebpackEscuelaIt/webpack-starter-kit/src/index.js
+++ WebpackEscuelaIt/webpack-starter-kit/src/index.js
@@ -1,3 +1,6 @@
 import './style.css'
+import './scss/main.scss'
+import data from './data.json' // A partir de Webpack4 no hace falta añadir un json-loader para esto
 
 console.log(...['hola', 'mundo'])
+console.log(data, data.name)
--- WebpackEscuelaIt/webpack-starter-kit/webpack.config.js
+++ WebpackEscuelaIt/webpack-starter-kit/webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssEsctracPlugin = require('mini-css-extract-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
     //entry:{},
@@ -23,15 +24,29 @@ module.exports = {
                 ]
             },
             {
-                test:/\.css$/,
+                test:/\.(css|scss)$/,
                 use: [
+                    'style-loader',
                     MiniCssEsctracPlugin.loader,
-                    'css-loader'
+                    'css-loader?minimize&sourceMap',
+                    'resolve-url-loader',
+                    'sass-loader?outputStyle=compressed&sourceMap'
                 ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg|webpg)$/i,
+                use: [
+                    'file-loader?name=assets/[name].[ext]'
+                ]
+            },
+            {
+                test: /\.(ttf|woff2?|mp4)$/i,
+                use: 'file-loader?name=assets/[name].[ext]'
             }
         ]
     },
     plugins: [
+        new CleanWebpackPlugin(['dist/**/*.*']),
         new HtmlWebpackPlugin({
             template:'./src/template.html',
             filename:'./index.html' // takes output folder as reference
